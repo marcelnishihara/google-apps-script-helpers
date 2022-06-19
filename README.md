67ebcb0a4a1478eda687973a49a87f61b1bea38a
Marcel Nishihara <marcelnishihara@gmail.com>
2022-06-19 20:48:47 -0300

    Class SpreadsheetReader Added

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# google_apps_script
+# Google Apps Script
+
+It is a compilation of classes, functions, and tips to help developers' routines easier when they working with Google Apps Script from Google Workspace (formerly G Suite).
+
+
+## Classes
+
+- `SpreadsheetReader`:
+  - Description: An easy-to-use class to help developers read Google Sheets file data from a given spreadsheet ID and sheet name in Google Apps Script context (Google Workspace);
+  - Properties:
+    - `sheetByName`: Sheet name to be read;
+    - `spreadsheetId`: Google Sheet File ID or the active spreadsheet.
+  - Methods:
+    - `spreadsheet`: The `spreadsheet` method returns the spreadsheet file opened by `spreadsheetId`;
+    - `sheet`: The `sheet` method returns the Class Sheet from a sheet opened by name;
+    - `sheetDataAsJson`: The `sheetDataAsJson` method transforms a table from a given sheet into a beautiful JSON valid object. It's normal to get emotional after running it.
+  - How to Use It:
+    ```js
+    > let sheet = new SpreadsheetReader('myAmazingSheet')
+    > let dataAsJson = sheet.sheetDataAsJson()
+    > console.info(dataAsJson)
+    [
+      { keyOne: 'valueFromCellA2', keyTwo: 'valueFromCellB2', keyThree: 'valueFromCellC2' },
+      { keyOne: 'valueFromCellA3', keyTwo: 'valueFromCellB3', keyThree: 'valueFromCellC3' },
+      { keyOne: 'valueFromCellA4', keyTwo: 'valueFromCellB4', keyThree: 'valueFromCellC4' }
+    ]
+    ```
+
+
+## Author
+- Marcel Nishihara, <marcelnishihara@gmail.com>
---
+++ SpreadsheetReader.js
@@ -0,0 +1,110 @@
+/**
+ * @class SpreadsheetReader
+ * @author Marcel Nishihara <marcelnishihara@gmail.com>
+ * @see {@link https://github.com/marcelnishihara/google_apps_script GitHub}
+ * @see {@link https://developers.google.com/apps-script Google Apps Script}
+ */
+ class SpreadsheetReader {
+    /**
+     * An easy-to-use class to help developers read Google Sheets
+     * file data from a given spreadsheet ID and sheet name in
+     * Google Apps Script context (Google Workspace).
+     * 
+     * Usually, the Google Sheets file ID can be extracted from the
+     * file URL, it is a bunch of characters between
+     * "https://docs.google.com/spreadsheets/d/" and "/edit#gid=0".
+     * When omitted, the `SpreadsheetReader` will try to read the 
+     * active spreadsheed.
+     * 
+     * The sheet name it is pretty self explained and it can be reached
+     * in the sheets menu at the bottom of the Google Sheets window.
+     * 
+     * @class SpreadsheetReader
+     * @param {string} sheetName Sheet name to be read.
+     * @param {string} [spreadsheetId] Google Sheet File ID or the active spreadsheet.
+     */
+    constructor(sheetName, spreadsheetId = SpradsheetApp.getActiveSpreadsheet()) {
+      this.spreadsheetId = SpreadsheetApp.openById(spreadsheetId)
+      this.sheetByName = this.spreadsheetId.getSheetByName(sheetName)
+    }
+  
+  
+    /**
+     * The `spreadsheet` method returns the spreadsheet file opened by `spreadsheetId`.
+     * 
+     * It is the same thing when you runs:
+     * ```
+     * SpreadsheetApp.openById(id)
+     * ```
+     * 
+     * @function spreadsheet
+     * @returns {SpreadsheetApp.Spreadsheet} Return of the openById(id) method.
+     * @see https://developers.google.com/apps-script/reference/spreadsheet/spreadsheet-app#openbyidid
+     */
+    spreadsheet() {
+      return this.spreadsheetId
+    }
+  
+  
+    /**
+     * The `sheet` method returns the Class Sheet from a sheet opened by name.
+     * 
+     * It is the same thing when you runs:
+     * ```
+     * SpreadsheetApp.openById(id).getSheetByName(name)
+     * ```
+     * 
+     * @function sheet
+     * @returns {SpreadsheetApp.Sheet} Return of the getSheetByName(name) method.
+     * @see https://developers.google.com/apps-script/reference/spreadsheet/sheet
+     */
+    sheet() {
+      return this.sheetByName
+    }
+  
+  
+    /**
+     * The `sheetDataAsJson` method transforms a table from a given sheet into
+     * a beautiful JSON valid object. It's normal to get emotional after running it.
+     * 
+     * This method contains only one optional parameter and it refers to the index
+     * of the row that you want that `sheetDataAsJson` assume as the keys (header) of
+     * each object. If omitted, the default value is zero.
+     * 
+     * Here's an example:
+     * ```
+     * > let sheet = new SpreadsheetReader('myAmazingSheet')
+     * > let dataAsJson = sheet.sheetDataAsJson()
+     * > console.info(dataAsJson)
+     * [
+     *  { keyOne: 'valueFromCellA2', keyTwo: 'valueFromCellB2', keyThree: 'valueFromCellC2' },
+     *  { keyOne: 'valueFromCellA3', keyTwo: 'valueFromCellB3', keyThree: 'valueFromCellC3' },
+     *  { keyOne: 'valueFromCellA4', keyTwo: 'valueFromCellB4', keyThree: 'valueFromCellC4' }
+     * ]
+     * ``` 
+     *
+     * @function sheetDataAsJson
+     * @param {number} [indexOfHeader] Index of the header.
+     * @returns {Array} Sheet data as a JSON valid object (array of objects).
+     */
+    sheetDataAsJson(indexOfHeader = 0) {
+      let dataAsJson = new Array()
+      let sheetData = this.sheetByName.getDataRange().getValues()
+      let ignoreHeader = indexOfHeader + 1
+  
+      sheetData.slice(ignoreHeader).forEach(function (row) {
+        let rowAsObject = new Object()
+  
+        row.forEach(function (cell, cellIndex) {
+          let objectKey = sheetData[indexOfHeader][cellIndex]
+          rowAsObject[objectKey] = cell
+        })
+  
+        dataAsJson.push(rowAsObject)
+  
+      })
+  
+      return dataAsJson
+  
+    }
+  }
